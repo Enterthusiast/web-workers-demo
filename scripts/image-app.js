afba1f3f4e7493ec0fd2440cc49a43f89cbc0dc9
Enterthusiast <bernard.damien@gmail.com>
2015-08-12 00:51:46 +0200

    Done Web Worker implementation & fixed the perf bug; however there is no error handling or code proofing

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -40,23 +40,25 @@
 
     toggleButtonsAbledness();
 
-    // Hint! This is where you should post messages to the web worker and
-    // receive messages from the web worker.
+    if (window.Worker) {
+      //check if Browser supports the Worker api.
+      // Requries script name as input
+      var myWorker = new Worker("scripts/worker.js");
+      var data = {
+        imageData: imageData,
+        type: type
+      };
+      myWorker.postMessage(data);
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+      myWorker.onmessage = function(e) {
+        imageData = e.data.imageData;
+        ctx.putImageData(imageData, 0, 0);
+        toggleButtonsAbledness();
+      }
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+
+    //toggleButtonsAbledness();
+    //return ctx.putImageData(imageData, 0, 0);
   };
 
   function revertImage() {
--- scripts/worker.js
+++ scripts/worker.js
@@ -3,21 +3,25 @@ importScripts('imageManips.js');
 this.onmessage = function(e) {
   var imageData = e.data.imageData;
   var type = e.data.type;
-
   try {
     length = imageData.data.length / 4;
+    var r, g, b, a, pixel;
+    var manipulateFunction = manipulate(type);
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
+      pixel = manipulateFunction(r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
       imageData.data[i * 4 + 3] = pixel[3];
     }
-    postMessage(imageData);
+    var data = {
+      imageData: imageData
+    };
+    postMessage(data);
   } catch (e) {
     function ManipulatorException(message) {
       this.name = "ManipulationException";
